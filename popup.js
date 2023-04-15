0ecf1877ec0cdbc3a25608d3fb3cee2e0f5fd413
Jasmin <jasmin_shah@live.com>
2023-04-15 23:02:24 +0000

    add all resolution images, introduce tailwind css

--- popup.js
+++ popup.js
@@ -2,12 +2,34 @@ document.getElementById('deleteCookies').addEventListener('click', () => {
   const domainsText = document.getElementById('domains').value;
   const domains = domainsText.split('\n');
   chrome.runtime.sendMessage({ action: 'deleteCookies', domains });
+
+  // Provide UI feedback
+  const deleteButton = document.getElementById('deleteCookies');
+  deleteButton.textContent = 'Deleted';
+  deleteButton.classList.remove('bg-red-500', 'hover:bg-red-600');
+  deleteButton.classList.add('bg-green-500', 'hover:bg-green-600');
+
+  setTimeout(() => {
+    deleteButton.textContent = 'Delete Cookies';
+    deleteButton.classList.remove('bg-green-500', 'hover:bg-green-600');
+    deleteButton.classList.add('bg-red-500', 'hover:bg-red-600');
+  }, 1000);
 });
 
 document.getElementById('saveDomains').addEventListener('click', () => {
   const domainsText = document.getElementById('domains').value;
   chrome.storage.local.set({ domains: domainsText }, () => {
-    alert('Domains saved successfully!');
+    // Provide UI feedback
+    const saveButton = document.getElementById('saveDomains');
+    saveButton.textContent = 'Saved';
+    saveButton.classList.remove('bg-indigo-600', 'hover:bg-indigo-700');
+    saveButton.classList.add('bg-green-500', 'hover:bg-green-600');
+
+    setTimeout(() => {
+      saveButton.textContent = 'Save Domains';
+      saveButton.classList.remove('bg-green-500', 'hover:bg-green-600');
+      saveButton.classList.add('bg-indigo-600', 'hover:bg-indigo-700');
+    }, 1000);
   });
 });
 

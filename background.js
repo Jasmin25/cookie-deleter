7766f9afd71455d19eb792f815179cb48ea69dbb
Jasmin <jasmin_shah@live.com>
2023-04-28 21:16:39 +0000

    added comments to all files

--- background.js
+++ background.js
@@ -1,17 +1,24 @@
+// Listener for messages from the popup script
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  // Check if the request type is 'deleteCookies'
   if (request.type === 'deleteCookies') {
     const domains = request.domains;
+    
+    // Loop through each domain and get cookies
     for (const domain of domains) {
       chrome.cookies.getAll({ domain }, (cookies) => {
+        // Loop through each cookie and remove it
         for (const cookie of cookies) {
           chrome.cookies.remove({ url: `http${cookie.secure ? 's' : ''}://${cookie.domain}${cookie.path}`, name: cookie.name });
         }
       });
     }
+    // Send a response indicating success
     sendResponse({ success: true });
   }
 });
 
+// Function to delete cookies for given domains
 function deleteCookiesForDomains(domains) {
   for (const domain of domains) {
     chrome.cookies.getAll({ domain }, (cookies) => {
@@ -22,11 +29,15 @@ function deleteCookiesForDomains(domains) {
   }
 }
 
+// Listener for when a tab is updated
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  // Check if the tab update status is 'complete'
   if (changeInfo.status === 'complete') {
+    // Get the saved domains from local storage
     chrome.storage.local.get('domains', (data) => {
       if (data.domains) {
         const domains = data.domains.split('\n');
+        // Delete cookies for the retrieved domains
         deleteCookiesForDomains(domains);
       }
     });
--- popup.js
+++ popup.js
@@ -1,14 +1,20 @@
+// Add click event listener for the "Delete Cookies" button
 document.getElementById('deleteCookies').addEventListener('click', () => {
+  // Get the domain list from the textarea
   const domainsText = document.getElementById('domains').value;
+  // Split the domain list into an array
   const domains = domainsText.split('\n');
+  // Send a message to background.js to delete cookies for the given domains
   chrome.runtime.sendMessage({ action: 'deleteCookies', domains });
 
   // Provide UI feedback
+  // Update the "Delete Cookies" button text and style to indicate cookies have been deleted
   const deleteButton = document.getElementById('deleteCookies');
   deleteButton.textContent = 'Deleted';
   deleteButton.classList.remove('bg-red-500', 'hover:bg-red-600');
   deleteButton.classList.add('bg-green-500', 'hover:bg-green-600');
 
+  // Revert the "Delete Cookies" button to its original state after 1 second
   setTimeout(() => {
     deleteButton.textContent = 'Delete Cookies';
     deleteButton.classList.remove('bg-green-500', 'hover:bg-green-600');
@@ -16,15 +22,20 @@ document.getElementById('deleteCookies').addEventListener('click', () => {
   }, 1000);
 });
 
+// Add click event listener for the "Save Domains" button
 document.getElementById('saveDomains').addEventListener('click', () => {
+  // Get the domain list from the textarea
   const domainsText = document.getElementById('domains').value;
+  // Save the domain list to local storage
   chrome.storage.local.set({ domains: domainsText }, () => {
     // Provide UI feedback
+    // Update the "Save Domains" button text and style to indicate the domains have been saved
     const saveButton = document.getElementById('saveDomains');
     saveButton.textContent = 'Saved';
     saveButton.classList.remove('bg-indigo-600', 'hover:bg-indigo-700');
     saveButton.classList.add('bg-green-500', 'hover:bg-green-600');
 
+    // Revert the "Save Domains" button to its original state after 1 second
     setTimeout(() => {
       saveButton.textContent = 'Save Domains';
       saveButton.classList.remove('bg-green-500', 'hover:bg-green-600');
@@ -33,6 +44,7 @@ document.getElementById('saveDomains').addEventListener('click', () => {
   });
 });
 
+// Retrieve the saved domain list from local storage and populate the textarea
 chrome.storage.local.get('domains', (data) => {
   if (data.domains) {
     document.getElementById('domains').value = data.domains;
